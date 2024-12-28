a25a61f4d475a0d9c40a414129c570a0dbd6c742
phani-ka <phaniinit@gmail.com>
2024-12-28 09:20:45 +0530

    add the golang

---
+++ golang_pra/gussgame.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	number := 50
+	guess := 50
+	if guess < number {
+		fmt.Println("Too low")
+	}
+	if guess > number {
+		fmt.Println("Too high")
+	}
+	if guess == number {
+		fmt.Println("You got it")
+	}
+	fmt.Println(number <= guess, number >= guess, number != guess)
+}
