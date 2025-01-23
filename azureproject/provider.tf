02e85ae613d02bdfc8405d4f69878329650c9c62
phani-ka <phaniinit@gmail.com>
2025-01-23 12:09:37 +0530

    Added the pdfs

---
+++ azureproject/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_version = ">= 1.11.0"
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = ">= 4.16.0"
+        }
+    }
+}
+
+
+provider "azurerm" {
+    
+}
---
+++ azureproject/variable.tf
@@ -0,0 +1,24 @@
+variable resource_group_name {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+
+variable resource_group_location {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable azure_blob_storage_name {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable azure_blob_container_name {
+  type        = string
+  default     = ""
+  description = "description"
+}
