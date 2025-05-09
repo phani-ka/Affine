a96cacbe81f4de03d2ed46d96674763b9489973c
phani-ka <phaniinit@gmail.com>
2025-05-09 13:07:32 +0530

    code add

--- linux_shellscript/logfilemovefor1m.sh
+++ linux_shellscript/logfilemovefor1m.sh
@@ -1,12 +1,6 @@
 #write a script to rotate log file to prevent them from growing too large.
 
 
-#!/bin/bash
-log_file="/var/log/logfile.log"
-maz_size=100000
 
-if [ $(wc -c < "$log_file") -gt $max_size ];
-then
-  mv "$log_file" "$log_file.old"
-  touch $logfile
-fi
+
+.
